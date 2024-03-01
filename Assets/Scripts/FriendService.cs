3d0969df79440757dda7ebcbb61bf3f71aba2c60
Igor Brusko <tailsmiles2015b@gmail.com>
2024-03-01 19:28:42 +0700

    Friends service

---
+++ Assets/Scripts/FriendService.cs
@@ -0,0 +1,25 @@
+﻿using Unity.Services.Authentication;
+using Unity.Services.Core;
+using Unity.Services.Friends;
+using UnityEngine;
+using Zenject;
+
+public class FriendService : IInitializable
+{
+    public async void Initialize()
+    {
+        // Initialize all of the used services
+        await UnityServices.InitializeAsync();
+
+        // Sign in anonymously or using a provider
+        await AuthenticationService.Instance.SignInAnonymouslyAsync();
+
+        // Initialize friends service
+        await FriendsService.Instance.InitializeAsync();
+
+        // Start using the Friends SDK functionalities.
+        var friends = FriendsService.Instance.Friends;
+        
+        Debug.Log($"<color=green>{GetType().Name} initalized</color>");
+    }
+}
--- Assets/Scripts/ZenjectIsntallers.cs
+++ Assets/Scripts/ZenjectIsntallers.cs
@@ -1,8 +1,3 @@
-using System;
-using System.Threading.Tasks;
-using Unity.Services.Authentication;
-using Unity.Services.Authentication.PlayerAccounts;
-using Unity.Services.Core;
 using UnityEngine;
 using UnityEngine.UI;
 using Zenject;
@@ -18,70 +13,6 @@ public class ZenjectInstallers : MonoInstaller
     }
 }
 
-public class AuthService : IInitializable
-{
-    public async void Initialize()
-    {
-        try
-        {
-            await UnityServices.InitializeAsync();
-            SetupEvents();
-        }
-        catch (Exception e)
-        {
-            Debug.LogException(e);
-        }
-        
-        Debug.Log($"<color=green>{GetType().Name} initalized</color>");
-    }
-    
-    private void SetupEvents()
-    {
-        AuthenticationService.Instance.SignedIn += SignedIn;
-
-        AuthenticationService.Instance.SignInFailed += (err) => {
-            Debug.LogError(err);
-        };
-
-        AuthenticationService.Instance.SignedOut += () => {
-            Debug.Log("Player signed out.");
-        };
-
-        AuthenticationService.Instance.Expired += () =>
-        {
-            Debug.Log("Player session could not be refreshed and expired.");
-        };
-    }
-
-    private async void SignedIn()
-    {
-        var accessToken = PlayerAccountService.Instance.AccessToken;
-        await SignInWithUnityAsync(accessToken);
-    }
-
-    private async Task InitSignIn()
-    { 
-        await PlayerAccountService.Instance.StartSignInAsync();
-    }
-    
-    async Task SignInWithUnityAsync(string accessToken)
-    {
-        try
-        {
-            await AuthenticationService.Instance.SignInWithUnityAsync(accessToken);
-            Debug.Log("SignIn is successful.");
-        }
-        catch (AuthenticationException ex)
-        {
-            Debug.LogException(ex);
-        }
-        catch (RequestFailedException ex)
-        {
-            Debug.LogException(ex);
-        }
-    }
-}
-
 public class CloudSaveService : IInitializable
 {
     public void Initialize()
@@ -91,14 +22,6 @@ public class CloudSaveService : IInitializable
 }
 
 public class PhotonService : IInitializable
-{
-    public void Initialize()
-    {
-        Debug.Log($"<color=green>{GetType().Name} initalized</color>");
-    }
-}
-
-public class FriendService : IInitializable
 {
     public void Initialize()
     {
