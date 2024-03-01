ff8e1331bb7d0b684d63ede4aef99de4b106b197
Igor Brusko <tailsmiles2015b@gmail.com>
2024-03-01 19:18:44 +0700

    Авторизация

--- Assets/Scripts/ZenjectIsntallers.cs
+++ Assets/Scripts/ZenjectIsntallers.cs
@@ -1,4 +1,10 @@
+using System;
+using System.Threading.Tasks;
+using Unity.Services.Authentication;
+using Unity.Services.Authentication.PlayerAccounts;
+using Unity.Services.Core;
 using UnityEngine;
+using UnityEngine.UI;
 using Zenject;
 
 public class ZenjectInstallers : MonoInstaller
@@ -14,9 +20,67 @@ public class ZenjectInstallers : MonoInstaller
 
 public class AuthService : IInitializable
 {
-    public void Initialize()
+    public async void Initialize()
     {
+        try
+        {
+            await UnityServices.InitializeAsync();
+            SetupEvents();
+        }
+        catch (Exception e)
+        {
+            Debug.LogException(e);
+        }
+
+        await InitSignIn();
+        
         Debug.Log($"<color=green>{GetType().Name} initalized</color>");
+    }
+    
+    private void SetupEvents()
+    {
+        AuthenticationService.Instance.SignedIn += SignedIn;
+
+        AuthenticationService.Instance.SignInFailed += (err) => {
+            Debug.LogError(err);
+        };
+
+        AuthenticationService.Instance.SignedOut += () => {
+            Debug.Log("Player signed out.");
+        };
+
+        AuthenticationService.Instance.Expired += () =>
+        {
+            Debug.Log("Player session could not be refreshed and expired.");
+        };
+    }
+
+    private async void SignedIn()
+    {
+        var accessToken = PlayerAccountService.Instance.AccessToken;
+        await SignInWithUnityAsync(accessToken);
+    }
+
+    private async Task InitSignIn()
+    { 
+        await PlayerAccountService.Instance.StartSignInAsync();
+    }
+    
+    async Task SignInWithUnityAsync(string accessToken)
+    {
+        try
+        {
+            await AuthenticationService.Instance.SignInWithUnityAsync(accessToken);
+            Debug.Log("SignIn is successful.");
+        }
+        catch (AuthenticationException ex)
+        {
+            Debug.LogException(ex);
+        }
+        catch (RequestFailedException ex)
+        {
+            Debug.LogException(ex);
+        }
     }
 }
 
