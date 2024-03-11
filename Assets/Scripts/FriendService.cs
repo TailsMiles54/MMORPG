2be361151ad1dfc1a0e9eefa3fd897fbc1ef66e7
Igor Brusko <tailsmiles2015b@gmail.com>
2024-03-11 21:53:24 +0700

    Удалил лишнее

--- Assets/Scripts/FriendService.cs
+++ Assets/Scripts/FriendService.cs
@@ -10,16 +10,12 @@ public class FriendService : IInitializable
     public event Action Initialized; 
     public async void Initialize()
     {
-        // Initialize all of the used services
         await UnityServices.InitializeAsync();
 
-        // Sign in anonymously or using a provider
         await AuthenticationService.Instance.SignInAnonymouslyAsync();
 
-        // Initialize friends service
         await FriendsService.Instance.InitializeAsync();
 
-        // Start using the Friends SDK functionalities.
         var friends = FriendsService.Instance.Friends;
         
         Debug.Log($"<color=green>{GetType().Name} initalized</color>");
