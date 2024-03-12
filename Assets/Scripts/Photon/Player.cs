33f621f2f05e7ccb6a02c1889e1932e579a33ca2
Igor Brusko <tailsmiles2015b@gmail.com>
2024-03-12 19:20:45 +0700

    Загрузка внешности

--- Assets/Scripts/Photon/Player.cs
+++ Assets/Scripts/Photon/Player.cs
@@ -1,13 +1,30 @@
+using System;
+using System.Linq;
 using Fusion;
 using Photon;
+using UnityEngine;
+using Zenject;
 
 public class Player : NetworkBehaviour
 {
+    [SerializeField] private CharacterAppearanceController _characterAppearanceController;
+    
     private NetworkCharacterController _cc;
+    
+    [Inject] private CloudSaveService _cloudSaveService;
+    [Inject] private CharacterService _characterService;
 
     private void Awake()
     {
         _cc = GetComponent<NetworkCharacterController>();
+    }
+
+    private async void Start()
+    {
+        var id = _characterService.CurrentCharacter;
+        var characters = await _cloudSaveService.LoadCharacters();
+        var characterData = characters.First(x => x.CharacterId == id);
+        _characterAppearanceController.SetupFromCharacterData(characterData.Gender, characterData.AppearanceSaveData);
     }
 
     public override void FixedUpdateNetwork()
